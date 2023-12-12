ab877a095e1bfe0b367b1d697baf1095354e1b3e
Tung <thanhtung.nguyen211192@gmail.com>
2023-12-12 21:02:29 +0700

    no message

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float horizontalInput;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        Move();
+        Jump();
+        Flip();
+    }
+
+    private void Flip()
+    {
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3 (-1,1,1);
+        }
+    }
+
+    private void Move()
+    {
+        if (horizontalInput != 0)
+        {
+            rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        } 
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
