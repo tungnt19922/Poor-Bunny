9ff9d56db447cbe303af7c0955a280f3538c13a7
DESKTOP-P83A4QD\thanh <thanhtung.nguyen211192@gmail.com>
2023-12-14 00:38:03 +0700

    no message

---
+++ Assets/Scripts/Trap_Arrow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_Arrow : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    public float spawnInterval = 3f;
+    public float startDelay = 10f;
+
+    void Start()
+    {
+        // Bắt đầu gọi hàm SpawnRandomArrow lặp lại sau mỗi 3 giây, bắt đầu từ giây thứ 10
+        InvokeRepeating("SpawnRandomArrow", startDelay, spawnInterval);
+    }
+
+    void SpawnRandomArrow()
+    {
+        // Tạo một vị trí ngẫu nhiên trong khoảng từ (-5,5) đến (5,5)
+        Vector2 randomPosition = new Vector2(Random.Range(-5f, 5f),5f);
+
+        // Tạo mũi tên tại vị trí ngẫu nhiên
+        Instantiate(arrowPrefab, randomPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Trap_Saw.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_Saw : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private Animator animator;
+    public Transform[] points; // Các điểm A, B, C, D
+    public float speed = 5f; // Tốc độ di chuyển
+    private int currentPointIndex = 0; // Index của điểm hiện tại
+    private int direction = 1; // Hướng di chuyển: 1 là chuyển từ điểm A đến D, -1 là chuyển từ D về A
+    private int sequenceStep = 0; // Bước trong chuỗi di chuyển
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        MoveObject();
+    }
+
+    void MoveObject()
+    {
+        // Kiểm tra xem đã đến điểm đích chưa
+        if (Vector2.Distance(transform.position, points[currentPointIndex].position) < 0.1f)
+        {
+            // Chuyển sang điểm tiếp theo hoặc trước đó tùy theo hướng
+            currentPointIndex += direction;
+
+            // Cập nhật bước trong chuỗi di chuyển
+            sequenceStep = (sequenceStep + direction + points.Length) % points.Length;
+
+            // Nếu đến điểm cuối cùng, thay đổi hướng
+            if (currentPointIndex == points.Length - 1 || currentPointIndex == 0)
+            {
+                direction *= -1;
+            }
+        }
+
+        // Di chuyển vật thể đến điểm đích
+        transform.position = Vector2.MoveTowards(transform.position, points[currentPointIndex].position, speed * Time.deltaTime);
+    }
+
+}
