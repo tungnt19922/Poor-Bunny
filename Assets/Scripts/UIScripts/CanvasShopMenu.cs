ab2a93e32854bfe6e91d6dd87708535c3eb272ad
Tung <thanhtung.nguyen211192@gmail.com>
2024-01-20 20:57:33 +0700

    no message

--- Assets/Scripts/UIScripts/CanvasShopMenu.cs
+++ Assets/Scripts/UIScripts/CanvasShopMenu.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class CanvasShopMenu : MonoBehaviour
 {
+    [SerializeField] GameObject[] skinPanel;
+    private int selectedPanel;
     [SerializeField] GameObject mainmenuPanel;
     [SerializeField] GameObject shopPanel;
 
+    private void Start()
+    {
+        skinPanel[selectedPanel].SetActive(true);
+    }
     public void BackButtonClicked()
     {
         mainmenuPanel.SetActive(true);
         shopPanel.SetActive(false);
+    }
+
+    public void NextPanel()
+    {
+        skinPanel[selectedPanel].SetActive(false);
+        selectedPanel++;
+        if (selectedPanel == skinPanel.Length)
+            selectedPanel = 0;
+        skinPanel[selectedPanel].SetActive(true);
+    }
+
+    public void PreviousPanel()
+    {
+        skinPanel[selectedPanel].SetActive(false);
+        selectedPanel--;
+        if (selectedPanel == -1)
+            selectedPanel = skinPanel.Length - 1;
+        skinPanel[selectedPanel].SetActive(true);
+
     }
 }
